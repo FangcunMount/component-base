af9adb71096751e0b9a288bd4ac9d7e2fe0ab558
yshujie <yshujie@163.com>
2025-12-03 10:48:31 +0800

    feat mongodb 数据库链接时，要求传入 Host 等数据库配置，而不是直接传入 URL

--- pkg/database/mongo.go
+++ pkg/database/mongo.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/url"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,12 +14,44 @@ import (
 
 // MongoConfig MongoDB 数据库配置
 type MongoConfig struct {
-	URL                      string `json:"url" mapstructure:"url"`
+	// 分离的连接参数（推荐使用，便于通过环境变量配置）
+	Host     string `json:"host,omitempty"     mapstructure:"host"`     // 主机地址，格式: host:port
+	Username string `json:"username,omitempty" mapstructure:"username"` // 用户名
+	Password string `json:"-"                  mapstructure:"password"` // 密码（不输出到JSON）
+	Database string `json:"database,omitempty" mapstructure:"database"` // 数据库名
+
 	UseSSL                   bool   `json:"use-ssl" mapstructure:"use-ssl"`
 	SSLInsecureSkipVerify    bool   `json:"ssl-insecure-skip-verify" mapstructure:"ssl-insecure-skip-verify"`
 	SSLAllowInvalidHostnames bool   `json:"ssl-allow-invalid-hostnames" mapstructure:"ssl-allow-invalid-hostnames"`
 	SSLCAFile                string `json:"ssl-ca-file" mapstructure:"ssl-ca-file"`
 	SSLPEMKeyfile            string `json:"ssl-pem-keyfile" mapstructure:"ssl-pem-keyfile"`
+}
+
+// BuildURL 根据配置参数构建 MongoDB 连接 URL
+func (c *MongoConfig) BuildURL() string {
+	// 构建基础 URL
+	scheme := "mongodb"
+	if c.UseSSL {
+		scheme = "mongodb+srv"
+	}
+
+	// 构建认证信息
+	var userInfo string
+	if c.Username != "" {
+		if c.Password != "" {
+			userInfo = fmt.Sprintf("%s:%s@", url.QueryEscape(c.Username), url.QueryEscape(c.Password))
+		} else {
+			userInfo = fmt.Sprintf("%s@", url.QueryEscape(c.Username))
+		}
+	}
+
+	// 构建数据库路径
+	dbPath := ""
+	if c.Database != "" {
+		dbPath = "/" + c.Database
+	}
+
+	return fmt.Sprintf("%s://%s%s%s", scheme, userInfo, c.Host, dbPath)
 }
 
 // MongoDBConnection MongoDB 连接实现
@@ -44,8 +77,11 @@ func (m *MongoDBConnection) Connect() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
+	// 根据配置构建连接 URL
+	mongoURL := m.config.BuildURL()
+
 	// 创建连接选项
-	clientOptions := options.Client().ApplyURI(m.config.URL)
+	clientOptions := options.Client().ApplyURI(mongoURL)
 
 	// 设置连接超时
 	clientOptions.SetConnectTimeout(5 * time.Second)
